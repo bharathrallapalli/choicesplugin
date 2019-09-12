f6ab747a2753d4a23777181bfb394c5dd0b4cef5
bharathrallapalli <bharathrallapalli@yahoo.in>
2019-09-12 13:11:15 +0530

    Inactive choices issues in properties dialog : fixed

--- src/main/resources/net/choices/WebContent/ChoicesPlugin.js
+++ src/main/resources/net/choices/WebContent/ChoicesPlugin.js
@@ -1,11 +1,35 @@
 require(["dojo/_base/declare",
         "dojo/_base/lang",
         "ecm/model/Desktop",
-        "dojo/_base/connect"
+        "dojo/_base/connect",
+        "ecm/widget/SinglePropertyEditorFactory",
+        "dojo/_base/array"
     ],
-    function(declare, lang, Desktop, connect) {
+    function(declare, lang, Desktop, connect, SinglePropertyEditorFactory, array) {
         console.log("Entered Choice Plugin "+new Date());
         connect.connect(Desktop, "onLogin", function() {
+
+            var superClassCreateEditorMethod = ecm.widget.SinglePropertyEditorFactory.prototype.createSinglePropertyEditor;
+            lang.extend(SinglePropertyEditorFactory,{
+                createSinglePropertyEditor: function(kwArgs, callback){
+                    if(kwArgs.choiceList && kwArgs.choiceList.choices && kwArgs.values && kwArgs.values.length>0 && kwArgs.cardinality === "SINGLE"){
+                        var value = kwArgs.values[0];
+                        var choices = kwArgs.choiceList.choices;
+                        var foundChoice = array.filter(choices, function(item){
+                            return item.value === value;
+                        });
+                        if(foundChoice.length<1){
+                            choices.push({
+                                value:value,
+                                name: value
+                            });
+                        }
+                    }
+                    return superClassCreateEditorMethod.apply(this,[kwArgs, callback]);
+                }
+            });
+
+
             var removeFeature = function(featureId) {
                 var featuresAvailable = ecm.model.desktop.features;
                 for (var j = 0; j < featuresAvailable.length; j++) {
